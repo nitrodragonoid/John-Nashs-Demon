969fbc527c7119d180b2a7742c116814d96041af
Hamad Abdul Razzaq <73271838+HamadAbdulRazzaq@users.noreply.github.com>
2021-05-08 03:37:14 +0500

    So now I made a 4x3 domineering game
    Here, I have finally created a game tree that contains all the possibilities. for this  i used a 4x3 list of lists and used the following conventions:
    - Empty space is denoted by a 0
    - Places filled by Player 1 is denoted by 1
    - Places filled by Player 2 is denoted by 2
    And following is the description of functions:
    - AddNodes and AddEdges simply adds the nodes and edges as we did in our DSA Labs
    - 4 Helper functions for queue
    - list_to_tuple converts the representation of our grid in list to tuple. This is done because we want diff states of grids to be nodes but since our graph is a dictionary, it can't 
       have any mutable datatype (including lists) as key. So to make them a key, I instead used a tuple form of our grid
    - player_determiner determines which player will be playing next.
    - is_full checks whether our grid has reached an end point or not. In terms of game tree, whether the current grid is a child or not
    - possible_combinations prints all next possible states of a grid from the current grid and the player number.
    - Driver code in the end basically helps in finding and storing all the nodes and edges and then creating a complete game tree for that. I have also printed all the nodes 
      (possibilities) in the end. Make sure to check it out !!
    Thats it !!

--- Domineering.py
+++ Domineering.py
@@ -1,13 +1,54 @@
 game_tree = dict()
-grid = [[0 for _ in range(3)] for __ in range(3)]
-def node_grid_maker(grid, p_no, x1, y1, x2, y2):
+grid = [[0 for _ in range(4)] for __ in range(3)]
+
+
+def addNodes(G, nodes):
+    for i in nodes:
+        G[i] = list()
+    return G
+
+
+def addEdges(G, edges, directed=False):
+    if directed:
+        for i, j, k in edges:
+            try:
+                G[i].append((j, k))
+            except:
+                return False
+    else:
+        for i, j, k in edges:
+            try:
+                G[i].append((j, k))
+                G[j].append((i, k))
+            except:
+                return False
+    return G
+
+
+def enQueue(queue, item):
+    queue.append(item)
+
+
+def deQueue(queue):
+    return queue.pop(0)
+
+
+def front(queue):
+    return queue[-1]
+
+
+def is_empty(queue):
+    return len(queue) == 0
+
+
+def list_to_tuple(grid):
     tmp = grid.copy()
-    tmp[x1][y1] = p_no
-    tmp[x2][y2] = p_no
     req = tuple()
     for i in tmp:
         req += (tuple(i),)
     return req
+
+
 def player_determiner(grid):
     count = 0
     for i in range(len(grid)):
@@ -18,6 +59,8 @@ def player_determiner(grid):
     if count % 2 == 1:
         return 2
     return 1
+
+
 def is_full(grid):
     for i in range(len(grid)):
         for j in range(len(grid[i])):
@@ -33,3 +76,54 @@ def is_full(grid):
                     if grid[i][j+1] == 0:
                         return False
     return True
+
+
+def possible_combinations(grid, p_no):
+    a = [list(i) for i in grid]
+    req = []
+    for i in range(len(a)):
+        for j in range(len(a[i])):
+            if a[i][j] == 0:
+                if i+1 < len(a) and a[i+1][j] == 0:
+                    new_comb = []
+                    for x in a:
+                        new_comb.append(x.copy())
+                    new_comb[i][j] = p_no
+                    new_comb[i+1][j] = p_no
+                    b = list_to_tuple(new_comb.copy())
+                    req.append(b)
+                if j+1 < len(a[i]) and a[i][j+1] == 0:
+                    new_comb = []
+                    for x in a:
+                        new_comb.append(x.copy())
+                    new_comb[i][j] = p_no
+                    new_comb[i][j+1] = p_no
+                    b = list_to_tuple(new_comb.copy())
+                    req.append(b)
+    return req
+
+
+nodes = []
+edges = []
+queue = []
+a = list_to_tuple(grid)
+enQueue(queue, a)
+nodes.append(a)
+while not(is_empty(queue)):
+    parent = deQueue(queue)
+    if is_full(parent):
+        continue
+    else:
+        p_no = player_determiner(parent)
+        possibilities = possible_combinations(parent, p_no)
+        for i in possibilities:
+            enQueue(queue, i)
+            nodes.append(i)
+            edges.append((parent, i, 1))
+addNodes(game_tree, nodes)
+addEdges(game_tree, edges, True)
+# Prinitng all possiblities i.e. Nodes
+for i in game_tree:
+    for j in i:
+        print(j)
+    print()
