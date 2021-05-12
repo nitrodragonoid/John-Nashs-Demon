baf1fe0f23611c01927cc0150db85764c4adf217
LakshmanMaheshwari <77591751+LakshmanMaheshwari@users.noreply.github.com>
2021-05-12 17:25:55 +0500

    Update Tictactoe.py

--- Tictactoe.py
+++ Tictactoe.py
@@ -149,3 +149,178 @@ def game_playing_x(initial):
         
 game_playing_x(board)
 
+
+\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+# GUI commit
+
+import pygame, sys
+import numpy as np
+pygame.init()
+
+# designing the tic tac toe game board
+WIDTH = 600
+HEIGHT = 600
+LINE_WIDTH = 15
+END_OF_GAME_LINE = 15
+ROWS = 3
+COLUMNS = 3
+MOVE_SPACE_SIZE = 200
+CIRCLE_RAD = 60
+CIRCLE = 15
+CROSS = 25
+SPACE = 55
+
+RED = (255, 0, 0)
+BG_COLOR = (20, 200, 160)
+LINE_COLOR = (23, 145, 135)
+CIRCLE_COLOR = (239, 231, 200)
+CROSS_COLOR = (66, 66, 66)
+
+screen = pygame.display.set_mode( (WIDTH, HEIGHT) )
+pygame.display.set_caption( 'TIC TAC TOE' )
+screen.fill( BG_COLOR )
+
+game_board = np.zeros( (ROWS, COLUMNS) )
+
+# drawslines on the game board
+def lines():
+	
+	pygame.draw.line( screen, LINE_COLOR, (0, MOVE_SPACE_SIZE), (WIDTH, MOVE_SPACE_SIZE), LINE_WIDTH )
+	
+	pygame.draw.line( screen, LINE_COLOR, (0, 2 * MOVE_SPACE_SIZE), (WIDTH, 2 * MOVE_SPACE_SIZE), LINE_WIDTH )
+
+	pygame.draw.line( screen, LINE_COLOR, (MOVE_SPACE_SIZE, 0), (MOVE_SPACE_SIZE, HEIGHT), LINE_WIDTH )
+
+	pygame.draw.line( screen, LINE_COLOR, (2 * MOVE_SPACE_SIZE, 0), (2 * MOVE_SPACE_SIZE, HEIGHT), LINE_WIDTH )
+
+# draws either a cross or a circle as a move depending on the player
+def shapes():
+	for row in range(ROWS):
+		for col in range(COLUMNS):
+			if game_board[row][col] == 1:
+				pygame.draw.circle( screen, CIRCLE_COLOR, (int( col * MOVE_SPACE_SIZE + MOVE_SPACE_SIZE//2 ), int( row * MOVE_SPACE_SIZE + MOVE_SPACE_SIZE//2 )), CIRCLE_RAD, CIRCLE )
+			elif game_board[row][col] == 2:
+				pygame.draw.line( screen, CROSS_COLOR, (col * MOVE_SPACE_SIZE + SPACE, row * MOVE_SPACE_SIZE + MOVE_SPACE_SIZE - SPACE), (col * MOVE_SPACE_SIZE + MOVE_SPACE_SIZE - SPACE, row * MOVE_SPACE_SIZE + SPACE), CROSS )	
+				pygame.draw.line( screen, CROSS_COLOR, (col * MOVE_SPACE_SIZE + SPACE, row * MOVE_SPACE_SIZE + SPACE), (col * MOVE_SPACE_SIZE + MOVE_SPACE_SIZE - SPACE, row * MOVE_SPACE_SIZE + MOVE_SPACE_SIZE - SPACE), CROSS )
+
+def square(row, col, player):
+	game_board[row][col] = player
+
+# checks if a move space is empty or not
+def vacant_move_space(row, col):
+	return game_board[row][col] == 0
+
+# checks if the board is full or not
+def game_board_full():
+	for row in range(ROWS):
+		for col in range(COLUMNS):
+			if game_board[row][col] == 0:
+				return False
+
+	return True
+
+# checks if there are 3 moves in succession on the board and then calls line functions to draw a line
+def check_3_moves(player):
+	for col in range(COLUMNS):
+		if game_board[0][col] == player and game_board[1][col] == player and game_board[2][col] == player:
+			win_line_verical(col, player)
+			return True
+
+	for row in range(ROWS):
+		if game_board[row][0] == player and game_board[row][1] == player and game_board[row][2] == player:
+			win_line_horizontal(row, player)
+			return True
+
+	if game_board[2][0] == player and game_board[1][1] == player and game_board[0][2] == player:
+		return True
+
+	if game_board[0][0] == player and game_board[1][1] == player and game_board[2][2] == player:
+		descending_diagonal_line(player)
+		return True
+
+	return False
+
+# draws a line if the 3 winning moves are in a vertical line
+def win_line_verical(col, player):
+	posX = col * MOVE_SPACE_SIZE + MOVE_SPACE_SIZE//2
+
+	if player == 1:
+		color = CIRCLE_COLOR
+	elif player == 2:
+		color = CROSS_COLOR
+
+	pygame.draw.line( screen, color, (posX, 15), (posX, HEIGHT - 15), LINE_WIDTH )
+
+# draws a line if the 3 winning moves are in a horizontal line
+def win_line_horizontal(row, player):
+	posY = row * MOVE_SPACE_SIZE + MOVE_SPACE_SIZE//2
+
+	if player == 1:
+		color = CIRCLE_COLOR
+	elif player == 2:
+		color = CROSS_COLOR
+
+	pygame.draw.line( screen, color, (15, posY), (WIDTH - 15, posY), END_OF_GAME_LINE )
+
+# draws a line if the 3 winning moves are in the ascending diagonal
+def ascending_diagonal_line(player):
+	if player == 1:
+		color = CIRCLE_COLOR
+	elif player == 2:
+		color = CROSS_COLOR
+
+	pygame.draw.line( screen, color, (15, HEIGHT - 15), (WIDTH - 15, 15), END_OF_GAME_LINE )
+
+# draws a line if the 3 winning moves are in the descending diagonal
+def descending_diagonal_line(player):
+	if player == 1:
+		color = CIRCLE_COLOR
+	elif player == 2:
+		color = CROSS_COLOR
+
+	pygame.draw.line( screen, color, (15, 15), (WIDTH - 15, HEIGHT - 15), END_OF_GAME_LINE )
+
+def restart():
+	screen.fill( BG_COLOR )
+	lines()
+	for row in range(ROWS):
+		for col in range(COLUMNS):
+			game_board[row][col] = 0
+
+# the defined game functions above are used to construct the interface below this line
+lines()
+
+player = 1
+game_over = False
+
+while True:
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			sys.exit()
+
+		if event.type == pygame.MOUSEBUTTONDOWN and not game_over:
+
+			X = event.pos[0] 
+			Y = event.pos[1] 
+
+			click_r = int(Y // MOVE_SPACE_SIZE)
+			click_c = int(X // MOVE_SPACE_SIZE)
+
+			if vacant_move_space( click_r, click_c ):
+
+				square( click_r, click_c, player )
+				if check_3_moves( player ):
+					game_over = True
+				player = player % 2 + 1
+
+				shapes()
+
+		if event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_r:
+				restart()
+				player = 1
+				game_over = False
+
+	pygame.display.update()
+    
+    \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
