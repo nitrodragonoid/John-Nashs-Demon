9aed4dcd31b7a3ebccd41bf940303e107479217b
Syed Mujtaba Hassan <70517236+nitrodragonoid@users.noreply.github.com>
2021-05-13 05:01:57 +0500

    Update Tictactoe.py

--- Tictactoe.py
+++ Tictactoe.py
@@ -65,7 +65,7 @@ def utility(state):
         return 0
     return False
 
-def action(state,turn):
+def action(state,turn): #outputs the list of all the possible moves
     out = []
     for i in range(len(state)):
         for j in range(len(state[i])):
@@ -74,6 +74,21 @@ def action(state,turn):
                 move[i][j] = turn
                 out.append(move) 
     return out
+
+def result1(state,move):
+    for i in range(len(move)):
+        for j in range(len(move[i])):
+            if move[i][j] != 0:
+                state[i][j] = move[i][j] 
+    return state
+
+def result2(state,move):
+    temp = state.copy()
+    for i in range(len(move)):
+        for j in range(len(move[i])):
+            if move[i][j] != 0:
+                temp[i][j] = move[i][j] 
+    return temp   
 
 def result(state, move):
     temp = []
@@ -84,7 +99,7 @@ def result(state, move):
             if move[i][j] != 0:
                 temp[i][j] = move[i][j]
     return temp
-    
+
 def check_turn(state):
     Xs = 0
     Os = 0
@@ -108,9 +123,8 @@ def max_value(state):
         return utility(state)
     v = -9999999
     for a in action(state,check_turn(state)):
-        if v < min_value(result(state,a)):
-            v = min_value(result(state,a))
-            best = a
+        check = state.copy()
+        v = max(v,min_value(result(check,a)))
     return v
 
 def min_value(state):
@@ -118,26 +132,28 @@ def min_value(state):
         return utility(state)
     v = 9999999
     for a in action(state,check_turn(state)):
-        if v > max_value(result(state,a)):
-            v = max_value(result(state,a))
-            best = a
+        check = state.copy()
+        v = min(v,max_value(result(check,a)))
     return v
 
 def MAX(state):
     val = -9999999
     for a in action(state,check_turn(state)):
-        if max_value(result(state,a)) > val:
-            val = max_value(result(state,a))
+        check = state.copy()
+        y = max_value(result(check,a))
+        if y > val:
+            val = y
             best = a
     return best
 
 def MIN(state):
     val = 9999999
     for a in action(state,check_turn(state)):
-        if min_value(result(state,a)) < val:
-            val = min_value(result(state,a))
+        check = state.copy()
+        x = min_value(result(check,a))
+        if x < val:
+            val = x
             best = a
-            print(best)
     return best
 
 def minimax(state):
@@ -183,7 +199,7 @@ def game_playing_x(initial):
         state = result(state,move)
         display(state)
         state = result(state,minimax(state))
-        display(state)
+    display(state)
     if utility(state) == 1:
         print("You won the game")
     elif utility(state) == -1:
