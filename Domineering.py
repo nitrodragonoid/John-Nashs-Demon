baa6555cd9926acb19b3a71757b194862fb75bdb
Hamad Abdul Razzaq <73271838+HamadAbdulRazzaq@users.noreply.github.com>
2021-05-17 18:26:18 +0500

    Console Based Domineering complete
    Just a slight note while running this version, enter the two indices as follows: <index1><space><index2>
    like:
    0 0
    0 1

--- Domineering.py
+++ Domineering.py
@@ -2,6 +2,18 @@ game_tree = dict()
 grid = [[0 for _ in range(4)] for __ in range(3)]
 
 
+def push(stack, item):
+    stack.append(item)
+
+
+def pop(stack):
+    return stack.pop()
+
+
+def top(stack):
+    return stack[-1]
+
+
 def addNodes(G, nodes):
     for i in nodes:
         G[i] = list()
@@ -88,19 +100,85 @@ def possible_combinations(grid, p_no):
                     new_comb = []
                     for x in a:
                         new_comb.append(x.copy())
-                    new_comb[i][j] = p_no
-                    new_comb[i+1][j] = p_no
+                    new_comb[i][j] = str(p_no) + "v"
+                    new_comb[i+1][j] = str(p_no) + "v"
                     b = list_to_tuple(new_comb.copy())
                     req.append(b)
                 if j+1 < len(a[i]) and a[i][j+1] == 0:
                     new_comb = []
                     for x in a:
                         new_comb.append(x.copy())
-                    new_comb[i][j] = p_no
-                    new_comb[i][j+1] = p_no
+                    new_comb[i][j] = str(p_no)+"h"
+                    new_comb[i][j+1] = str(p_no) + "h"
                     b = list_to_tuple(new_comb.copy())
                     req.append(b)
     return req
+
+
+def update_grid(grid, idx1, idx2, p_no):
+    while True:
+        a = idx1.split()
+        a = [int(i) for i in a]
+        b = idx2.split()
+        b = [int(i) for i in b]
+        if (a[0]+1 == b[0] and a[1] == b[1]):
+            break
+        elif (a[0] == b[0] and a[1]+1 == b[1]):
+            break
+        idx1 = int(input("Enter Valid Index 1: "))
+        idx2 = int(input("Enter Valid Index 2: "))
+    if (a[0]+1 == b[0] and a[1] == b[1]):
+        grid[a[0]][a[1]] = str(p_no)+"v"
+        grid[b[0]][b[1]] = str(p_no)+"v"
+    elif (a[0] == b[0] and a[1]+1 == b[1]):
+        grid[a[0]][a[1]] = str(p_no)+"h"
+        grid[b[0]][b[1]] = str(p_no)+"h"
+
+
+def display_grid(grid):
+    for i in grid:
+        print(i)
+
+
+def dfs(graph, current_state, comp_no, visited, parent):
+    visited.append(current_state)
+    for i in graph[current_state]:
+        if not(i[0] in visited):
+            parent[i[0]] = current_state
+            if len(graph[i[0]]) == 0 and player_determiner(current_state) != comp_no:
+                return
+            else:
+                dfs(graph, i[0], comp_no, visited, parent)
+
+
+def bot(grid, p_no, graph):
+    visited = []
+    parent = dict()
+    temp = list_to_tuple(grid.copy())
+    parent[temp] = temp
+    dfs(graph, temp, p_no, visited, parent)
+    a = tuple()
+    for i in visited:
+        if len(graph[i]) == 0:
+            a = i
+            break
+    sequence = list()
+    while a != temp:
+        sequence.append(a)
+        a = parent[a]
+    sequence.append(temp)
+    return sequence[-2]
+
+
+def tuple_to_list(grid):
+    lst = []
+    for i in grid:
+        lst.append(list(i))
+    return lst
+
+
+test1 = [['1h', '1h', 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
+count = 0
 
 
 nodes = []
@@ -122,8 +200,21 @@ while not(is_empty(queue)):
             edges.append((parent, i, 1))
 addNodes(game_tree, nodes)
 addEdges(game_tree, edges, True)
-# Prinitng all possiblities i.e. Nodes
-for i in game_tree:
-    for j in i:
-        print(j)
-    print()
+count = 0
+last_turn = 0
+
+while not(is_full(grid)):
+    p_no = player_determiner(grid)
+    display_grid(grid)
+    print(f"Player {p_no} turn.")
+    if count % 2 == 0:
+        idx1 = input("Enter Index 1: ")
+        idx2 = input("Enter Index 2: ")
+        update_grid(grid, idx1, idx2, p_no)
+    elif count % 2 != 0:
+        x = bot(grid, player_determiner(test1), game_tree)
+        grid = tuple_to_list(x).copy()
+    last_turn = p_no
+    count += 1
+display_grid(grid)
+print(f"Game Over! Player {p_no} Won !!")
