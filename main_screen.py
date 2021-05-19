f6402f6f00ad51cfda4fafc1669bde369eec7c09
LakshmanMaheshwari <77591751+LakshmanMaheshwari@users.noreply.github.com>
2021-05-19 22:31:18 +0500

    Update main_screen.py

--- main_screen.py
+++ main_screen.py
@@ -4,7 +4,7 @@ pygame.init()
 
 # a function for making buttons
 def button(screen, position, text):
-    font = pygame.font.SysFont('timesnewroman', 35)
+    font = pygame.font.SysFont('timesnewroman', 50)
     text_render = font.render(text, 1, (150, 100, 100))
     x, y, width , height = text_render.get_rect()
     x, y = position
@@ -12,27 +12,29 @@ def button(screen, position, text):
     pygame.draw.line(screen, (0, 0, 150), (x, y - 2), (x, y + height), 5)
     pygame.draw.line(screen, (0, 0, 150), (x, y + height), (x + width , y + height), 5)
     pygame.draw.line(screen, (0, 0, 150), (x + width , y + height), [x + width , y], 5)
-    pygame.draw.rect(screen, (0, 255, 255), (x, y, width , height))
+    pygame.draw.rect(screen, (0, 0, 0), (x, y, width , height))
     return screen.blit(text_render, (x, y))
 
 # making the main screen back ground
-main_screen = pygame.display.set_mode((600, 400))
+main_screen = pygame.display.set_mode((640, 570))
 pygame.display.set_caption( 'Pick the Block' )
-BG_COLOR = (100, 200, 160)
+BG_COLOR = (0, 0, 0)
 main_screen.fill( BG_COLOR )
 
 # main function for the screen
 def mainmenu():
 
-    pygame.draw.ellipse(main_screen, (180, 255, 180), [175, 20, 225, 100], 0)
-    font = pygame.font.SysFont('timesnewroman', 20)
-    text = font.render("#Some Catchphrase",True,(150, 100, 100))
+    pygame.draw.ellipse(main_screen, (180, 255, 180), [70, 20, 500, 150], 0)
+    font = pygame.font.SysFont('timesnewroman', 50)
+    text = font.render("John Nash's Demon",True,(150, 100, 100))
     textRect = text.get_rect()
-    textRect.center = (288, 70)
+    textRect.center = (320, 85)
     main_screen.blit(text,textRect)
-    tic_tac_toe = button(main_screen, (205, 150), "Tic Tac Toe")
-    domineering = button(main_screen, (192, 250), "Domineering")
-    pick_the_brick = button(main_screen,(185,350), "Pick the Brick")
+    hex = button(main_screen,(280,200), "Hex")
+    tic_tac_toe = button(main_screen, (205, 300), "Tic Tac Toe")
+    domineering = button(main_screen, (193, 400), "Domineering")
+    pick_the_brick = button(main_screen,(180,500), "Pick the Brick")
+
 
     while True:
         for event in pygame.event.get():
@@ -45,12 +47,14 @@ def mainmenu():
             # checks if the mouse has been clicked 
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if tic_tac_toe.collidepoint(pygame.mouse.get_pos()):
-                    pass # edit here
+                    pass
                 elif domineering.collidepoint(pygame.mouse.get_pos()):
-                    pass # edit here
+                    pass
                 elif pick_the_brick.collidepoint(pygame.mouse.get_pos()):
-                    pass # edit here
+                    pass
+                elif hex.collidepoint(pygame.mouse.get_pos()):
+                    pass
         pygame.display.update()
-    pygame.quit()
+
 
 mainmenu()
