5a3553c122584cb2d720c77a3df647a554a8faad
LakshmanMaheshwari <77591751+LakshmanMaheshwari@users.noreply.github.com>
2021-05-20 01:18:13 +0500

    Update all_games_combined.py

--- all_games_combined.py
+++ all_games_combined.py
@@ -381,8 +381,8 @@ def Domineering():
         pygame.display.update()
 
 
-""" Pick the brick code """
-def Pick_The_Brick():
+""" Pick the block code """
+def Pick_The_Block():
     """ GUI for the Game"""  # integrated with the bot
     # making the main screen
     screen = pygame.display.set_mode((640, 570))
@@ -1118,9 +1118,11 @@ def button(screen, position, text):
 
 # making the main screen back ground
 main_screen = pygame.display.set_mode((640, 570))
-pygame.display.set_caption( 'Pick the Block' )
+pygame.display.set_caption( "Game Bot" )
 BG_COLOR = (0, 0, 0)
 main_screen.fill( BG_COLOR )
+
+
 
 # main function for the screen
 def mainmenu():
@@ -1129,12 +1131,12 @@ def mainmenu():
     font = pygame.font.SysFont('timesnewroman', 50)
     text = font.render("John Nash's Demon",True,(150, 100, 100))
     textRect = text.get_rect()
-    textRect.center = (320, 85)
+    textRect.center = (320, 90)
     main_screen.blit(text,textRect)
-    hex = button(main_screen,(280,200), "Hex")
-    tic_tac_toe = button(main_screen, (205, 300), "Tic Tac Toe")
+    hex = button(main_screen,(282,200), "Hex")
+    tic_tac_toe = button(main_screen, (207, 300), "Tic Tac Toe")
     domineering = button(main_screen, (193, 400), "Domineering")
-    pick_the_brick = button(main_screen,(180,500), "Pick the Brick")
+    pick_the_brick = button(main_screen,(177,500), "Pick the Block")
 
 
     while True:
@@ -1152,7 +1154,7 @@ def mainmenu():
                 elif domineering.collidepoint(pygame.mouse.get_pos()):
                     Domineering()
                 elif pick_the_brick.collidepoint(pygame.mouse.get_pos()):
-                    Pick_The_Brick()
+                    Pick_The_Block()
                 elif hex.collidepoint(pygame.mouse.get_pos()):
                     pass
         pygame.display.update()
